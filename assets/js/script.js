69202b49bfc0819b0fd62457d427c8421f79d4e2
AmanPunia-510 <puniaaman888@gmail.com>
2024-04-05 19:39:31 +0530

    work on toggle ball

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,12 @@
-let toggle_box = document.querySelector(".toggle-ball");
-
+const toggle_box = document.querySelector(".toggle-ball");
+const toggle_ball = document.querySelector(".toggle-ball");
 toggle_box.addEventListener("click", function () {
-    toggle_box.classList.toggle("toggle-ball-right")
-})
+  toggle_ball.classList.toggle("toggle-ball-right");
+});
 
-
-let menuicons = document.querySelector("#menubar")
-let navlist = document.querySelector(".nav-items")
+let menuicons = document.querySelector("#menubar");
+let navlist = document.querySelector(".nav-items");
 
 menuicons.addEventListener("click", () => {
-    navlist.classList.toggle("show");
-})
+  navlist.classList.toggle("show");
+});
